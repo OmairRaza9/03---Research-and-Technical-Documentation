091aa088a07698f151c08cc3c8ff5fa8e1e46498
Omair <omairraza9@gmail.com>
2017-10-07 18:52:27 -0400

    first commit

---
+++ research_and_technical_documentation_921.rb
@@ -0,0 +1,42 @@
+# string = "    One    two   three   "
+# => "    One    two   three   "
+#
+# string.split
+# => ["One", "two", "three"]
+#
+# string.strip
+# => "One    two   three"
+#
+#  string.length
+# => 25
+#
+#
+#
+# array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# => [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+#
+#  array.first  #first item in the array
+# => 1
+# array.delete_at(0)   #deletes the item in teh value you write in the ()
+# => 1
+# array.delete(9) #Deletes the value you type
+# => 9
+#  array.pop   #Deletes the last item
+# => 0
+#  array
+# => [2, 3, 4, 5, 6, 7, 8]
+#
+# hash = {
+#   1 => "a",
+#   2 => "b",
+#   3 => "c"
+# }
+#
+# puts hash.to_a
+# puts hash.has_key?(4)
+# puts hash.has_key?("a")
+# puts hash.has_value?(1)
+# puts hash.has_value?("a")
+
+
+#p Time.now
